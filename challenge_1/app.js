6f8d19956c2bb99ada37f8b93cc57976021c8587
Joanna Wang <shiju.an362@gmail.com>
2020-03-02 11:58:55 -0800

    fix onclick to change values properly

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,78 +1,68 @@
 console.log('what is happening in life');
 
-
-// fucntion insertChoice (choice) {
-
-// }
-
 const td = document.querySelectorAll('td');
-const table = document.querySelector('table');
 let tdCount = 0;
-
-// function countTdClick() {
-//   console.log(td);
-//   // for (let i = 0; i < td.length; i++) {
-//   //   td[i].addEventListener("click", function () {
-//   //     console.log('td clicked');
-//   //     tdCount++;
-//   //     console.log(tdCount,'inner');
-//   //   });
-//   // }
-
-
-// }
-
-// table.addEventListener("click", function() {
-//   console.log('table clicked');
-//   tdCount ++;
-//   console.log(tdCount, 'inner');
-// })
-
-console.log(tdCount, 'global');
 
 for (let i = 0; i < td.length; i++) {
   td[i].addEventListener("click", function () {
-    console.log('td clicked');
-    tdCount++;
+    // console.log('td clicked');
+    // tdCount++;
+    tdCount ++;
     changeValue();
-  }, false);
+  });
+
+function changeXValue() {
+  td[i].innerHTML = 'X';
+  // tdCount ++;
 }
 
-  function changeValue() {
-    for (let i = 0; i < td.length; i++) {
-      if (tdCount % 2 === 1) {
-        td[i].onclick = function changeXValue() {
-          td[i].innerHTML = 'X';
-        }
-      }
+function changeOValue() {
+  td[i].innerHTML = 'O';
+  // tdCount ++;
+}
 
-      if (tdCount % 2 === 0) {
-        td[i].onclick = function changeOValue() {
-          td[i].innerHTML = 'O';
-        }
-      }
+function changeValue() {
+  // for (let i = 0; i < td.length; i++) {
+    if (tdCount % 2 === 1) {
+      td[i].onclick =
+      // function changeXValue() {
+      //   td[i].innerHTML = 'X';
+      //   tdCount ++;
+      // }
+      changeXValue();
     }
+
+    if (tdCount % 2 === 0) {
+      td[i].onclick =
+      // function changeOValue() {
+      //   td[i].innerHTML = 'O';
+      //   tdCount ++;
+      // }
+      changeOValue();
+    // }
   }
-
-//   td.addEventListener("click", function() {
-//     console.log('td clicked');
-//     for (let i = 0; i < td.length; i++) {
-//       console.log(td[i].innerHTML)
-//     }
-//   })
-// }
-
-// countTdClick();
-// const structure = document.getElementById("structure");
-// structure.addEventListener("click", function() {
-//   console.log('clicked');
-//   structure.innerHTML = console.log(`Click count:  event.detail`);
-// }, false);
+}
+}
+console.log(tdCount, 'global');
 
 
+  //   td.addEventListener("click", function() {
+  //     console.log('td clicked');
+  //     for (let i = 0; i < td.length; i++) {
+  //       console.log(td[i].innerHTML)
+  //     }
+  //   })
+  // }
 
-// count main clicks
-// if odd = X
-// if even  O
+  // countTdClick();
+  // const structure = document.getElementById("structure");
+  // structure.addEventListener("click", function() {
+  //   console.log('clicked');
+  //   structure.innerHTML = console.log(`Click count:  event.detail`);
+  // }, false);
 
-``
+
+
+  // count main clicks
+  // if odd = X
+  // if even  O
