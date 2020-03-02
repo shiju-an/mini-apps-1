1c46a5a6d61961d2023b4dca1f7109b00fcf9202
Joanna Wang <shiju.an362@gmail.com>
2020-03-02 10:49:30 -0800

    create html table and attempt to implement click counter

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,31 @@
+console.log('what is happening in life');
 
+var tdCount = 0;
 
+// fucntion insertChoice (choice) {
+
+// }
+
+function countTdClick () {
+  const td = document.querySelectorAll('td');
+  console.log(td);
+  for (let i = 0; i < td.length; i++) {
+    td[i].addEventListener("click", function() {
+      console.log('td clicked');
+    }
+  }
+
+//   td.addEventListener("click", function() {
+//     console.log('td clicked');
+//     for (let i = 0; i < td.length; i++) {
+//       console.log(td[i].innerHTML)
+//     }
+//   })
+// }
+
+countTdClick();
+// const structure = document.getElementById("structure");
+// structure.addEventListener("click", function() {
+//   console.log('clicked');
+//   structure.innerHTML = console.log(`Click count:  event.detail`);
+// }, false);
