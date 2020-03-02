abe0bbdb38423a5a91da2aced4337947c4b426bf
Joanna Wang <shiju.an362@gmail.com>
2020-03-02 11:24:56 -0800

    change values for td on click, counter not working properly

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -6,34 +6,53 @@ console.log('what is happening in life');
 // }
 
 const td = document.querySelectorAll('td');
-let tdCount = 1;
+const table = document.querySelector('table');
+let tdCount = 0;
 
-function countTdClick() {
-  console.log(td);
-  for (let i = 0; i < td.length; i++) {
-    td[i].addEventListener("click", function () {
-      console.log('td clicked');
-      tdCount++;
-      console.log(tdCount);
-    });
-  }
-}
+// function countTdClick() {
+//   console.log(td);
+//   // for (let i = 0; i < td.length; i++) {
+//   //   td[i].addEventListener("click", function () {
+//   //     console.log('td clicked');
+//   //     tdCount++;
+//   //     console.log(tdCount,'inner');
+//   //   });
+//   // }
 
-console.log(tdCount);
+
+// }
+
+// table.addEventListener("click", function() {
+//   console.log('table clicked');
+//   tdCount ++;
+//   console.log(tdCount, 'inner');
+// })
+
+console.log(tdCount, 'global');
 
 for (let i = 0; i < td.length; i++) {
-  if (tdCount % 2 === 1) {
-    td[i].onclick = function changeXValue() {
-      td[i].innerHTML = 'X';
-    }
-  }
+  td[i].addEventListener("click", function () {
+    console.log('td clicked');
+    tdCount++;
+    changeValue();
+  }, false);
+}
 
-  if (tdCount % 2 === 0) {
-    td[i].onclick = function changeOValue() {
-      td[i].innerHTML = 'O';
+  function changeValue() {
+    for (let i = 0; i < td.length; i++) {
+      if (tdCount % 2 === 1) {
+        td[i].onclick = function changeXValue() {
+          td[i].innerHTML = 'X';
+        }
+      }
+
+      if (tdCount % 2 === 0) {
+        td[i].onclick = function changeOValue() {
+          td[i].innerHTML = 'O';
+        }
+      }
     }
   }
-};
 
 //   td.addEventListener("click", function() {
 //     console.log('td clicked');
@@ -43,7 +62,7 @@ for (let i = 0; i < td.length; i++) {
 //   })
 // }
 
-countTdClick();
+// countTdClick();
 // const structure = document.getElementById("structure");
 // structure.addEventListener("click", function() {
 //   console.log('clicked');
