de6f472d171d655f6724294882770dfaaa57074f
Joanna Wang <shiju.an362@gmail.com>
2020-03-04 15:18:31 -0800

    prepare dev enviornment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default App;
+
+console.log('hi');
+console.log('cry');
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('HEY LISTEN @ 3000');
+})
 
 
