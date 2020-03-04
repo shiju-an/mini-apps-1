e10ba71e980b99ab024b55f862e6de7bd38be34c
Joanna Wang <shiju.an362@gmail.com>
2020-03-04 11:10:18 -0800

    work on rendering form + post data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
+const express = require('express');
+const bodyParser = require('body-parser');
+// const http = require('http');
+const path = require('path');
 const app = express();
 
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.engine('ejs', require('ejs').renderFile);
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -10,12 +14,22 @@ app.use(bodyParser.json())
 app.get('/upload_json', (req, res) => {
   console.log('is this getting');
   // res.render('form');
-  res.send('got ya');
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
 app.post('/upload_json', (req, res) => {
   console.log('are ya posting data ', req.body.JSONdata);
-  res.send('sup post');
+  // if (req.body.JSONdata) {
+  //   res.render('/Users/ohjeezz/Documents/Hack Reactor/hrsf126-mini-apps-1/challenge_2/client/index.html', {
+  //     csv: req.body.JSONdata,
+  //   })
+  // } else {
+  //   res.redirect('index.html');
+  const response = { csv: req.body.JSONdata };
+  console.log(response);
+  res.end(JSON.stringify(response));
+  // res.redirect('index.html');
+  // res.send('sup post');
 })
 
 
